32409c11b0380ba013feacce99eb53a9b8af7196
VALER - K0NNK <98103125+VALER24@users.noreply.github.com>
2025-02-05 19:19:34 -0600

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,7 +2,7 @@
 
 # install asl3 and allmon3
 
-apt update && apt upgrade && apt install git apt install wget -y
+apt update && apt upgrade && apt install git && apt install wget -y
 cd /tmp
 wget https://repo.allstarlink.org/public/asl-apt-repos.deb12_all.deb
 dpkg -i asl-apt-repos.deb12_all.deb
